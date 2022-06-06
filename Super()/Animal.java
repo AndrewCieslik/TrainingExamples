8049ca09745a624e696f5db61d709d9bb9e1659b
Andrew <101130154+AndrewCieslik@users.noreply.github.com>
2022-06-06 19:47:11 +0200

    Delete Super() directory

--- Super()/Animal.java
+++
@@ -1,9 +0,0 @@
-public abstract class Animal {
-    String family;
-    String environment;
-
-    public Animal(String family, String environment) {
-        this.family = family;
-        this.environment = environment;
-    }
-}
--- Super()/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Fish greatWhite = new Fish("shark", "ocean");
-        greatWhite.setSound("aHHGHHrrrr");
-        greatWhite.makeSound();
-    }
-}
